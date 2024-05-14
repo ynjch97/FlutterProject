d46bc8a4c06de0d7759cefe15605dccd70cf1357
ynjch97 <ynjch2017@naver.com>
2024-05-14 15:25:31 +0900

    WEBTOON APP > Webtoon Card

--- toonflix/lib/screens/home_screen.dart
+++ toonflix/lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
 import 'package:toonflix/models/webtoon_model.dart';
 import 'package:toonflix/services/api_service.dart';
 
@@ -34,20 +35,21 @@ class HomeScreen extends StatelessWidget {
         builder: (context, snapshot) {
           if (snapshot.hasData) {
             // snapshot 이 데이터를 가지고 있는 경우
-            return ListView.separated(
-              scrollDirection: Axis.horizontal, // 스크롤 방향
-              itemCount: snapshot.data!.length,
-              itemBuilder: (context, index) {
-                var webtoon = snapshot.data![index];
-                return Text('$index  ${webtoon.title}');
-              },
-              separatorBuilder: (context, index) {
-                return const SizedBox(
-                  width: 20,
-                );
-              },
+            return Column(
+              children: [
+                const SizedBox(
+                  height: 50,
+                ),
+                Expanded(
+                  child: makeList(snapshot),
+                )
+                // The following assertion was thrown during performResize
+                // ListView 에 설정된 높이가 없음 -> 제한 없는 높이값이 넘어옴 -> Expanded() 로 해결
+              ],
             );
           }
+
+          // 아직 값이 안넘어온 상태 = 로딩 중
           return const Center(
             child: CircularProgressIndicator(
               color: Color(0xFF00DC64),
@@ -55,6 +57,59 @@ class HomeScreen extends StatelessWidget {
           );
         },
       ),
+    );
+  }
+
+  ListView makeList(AsyncSnapshot<List<WebtoonModel>> snapshot) {
+    return ListView.separated(
+      scrollDirection: Axis.horizontal, // 스크롤 방향
+      itemCount: snapshot.data!.length, // 몇 개의 아이템을 build 할지
+      padding: const EdgeInsets.symmetric(
+        horizontal: 20,
+        vertical: 10,
+      ),
+      itemBuilder: (context, index) {
+        // 데이터가 있다고 확신하기 위해 ! 기호 사용하여 오류 제거
+        var webtoon = snapshot.data![index];
+        return Column(
+          children: [
+            Container(
+              width: 250,
+              clipBehavior: Clip.hardEdge,
+              // BorderRadius.circular 적용이 되지 않는 이유 : clipBehavior 때문
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                boxShadow: [
+                  BoxShadow(
+                    blurRadius: 7,
+                    offset: const Offset(3, 3),
+                    color: Colors.black.withOpacity(0.5),
+                  )
+                ],
+              ),
+              child: Image.network(
+                webtoon.thumb,
+              ),
+            ),
+            const SizedBox(
+              height: 10,
+            ),
+            Text(
+              webtoon.title,
+              style: const TextStyle(
+                fontSize: 22,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ],
+        );
+      },
+      separatorBuilder: (context, index) {
+        // 아이템 사이 렌더링 될 Widget 설정
+        return const SizedBox(
+          width: 40,
+        );
+      },
     );
   }
 }
