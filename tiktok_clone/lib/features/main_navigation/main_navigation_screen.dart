9d4f0418894b5be2e5b0008bb111fdfb619f8962
ynjch97 <ynjch2017@naver.com>
2024-05-28 11:47:00 +0900

    틱톡 클론코딩 > TAB NAVIGATION > Custom NavigationBar

--- tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
+++ tiktok_clone/lib/features/main_navigation/main_navigation_screen.dart
@@ -1,6 +1,8 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
+
+import 'widgets/nav_tab.dart';
 
 class MainNavigationScreen extends StatefulWidget {
   const MainNavigationScreen({super.key});
@@ -14,10 +16,10 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   final screens = const [
     Center(child: Text("Home")),
-    Center(child: Text("Search")),
-    Center(child: Text("Home")),
-    Center(child: Text("Search")),
-    Center(child: Text("Home"))
+    Center(child: Text("Discover")),
+    Center(child: Text("+")),
+    Center(child: Text("Inbox")),
+    Center(child: Text("Profile"))
   ];
 
   void _onNavTap(int index) {
@@ -28,21 +30,46 @@ class _MainNavigationScreenState extends State<MainNavigationScreen> {
 
   @override
   Widget build(BuildContext context) {
-    // MaterialApp 도 CupertinoApp 으로 변경해주어야 함
-    return CupertinoTabScaffold(
-      tabBar: CupertinoTabBar(
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.house),
-            label: "Home",
-          ),
-          BottomNavigationBarItem(
-            icon: Icon(CupertinoIcons.search),
-            label: "Search",
-          ),
-        ],
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("MainNavigationScreen"),
       ),
-      tabBuilder: (context, index) => screens[index],
+      body: screens[_selectedIndex],
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: Sizes.size10),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              NavTab(
+                icon: FontAwesomeIcons.house,
+                text: "Home",
+                isSelected: _selectedIndex == 0,
+                onTap: () => _onNavTap(0),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.magnifyingGlass,
+                text: "Discover",
+                isSelected: _selectedIndex == 1,
+                onTap: () => _onNavTap(1),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.inbox,
+                text: "Inbox",
+                isSelected: _selectedIndex == 3,
+                onTap: () => _onNavTap(3),
+              ),
+              NavTab(
+                icon: FontAwesomeIcons.user,
+                text: "Profile",
+                isSelected: _selectedIndex == 4,
+                onTap: () => _onNavTap(4),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
---
+++ tiktok_clone/lib/features/main_navigation/widgets/nav_tab.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+
+class NavTab extends StatelessWidget {
+  final String text;
+  final bool isSelected;
+  final IconData icon;
+  final Function onTap;
+
+  const NavTab({
+    super.key,
+    required this.text,
+    required this.isSelected,
+    required this.icon,
+    required this.onTap,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      // 영역 확장을 위해 Expanded + Container 활용
+      child: GestureDetector(
+        onTap: () => onTap(),
+        child: Container(
+          color: Colors.black,
+          child: AnimatedOpacity(
+            duration: const Duration(milliseconds: 300),
+            opacity: isSelected ? 1 : 0.6,
+            child: Column(
+              // Column 은 세로축으로 최대한 확장하려고 하기 때문에, children 의 공간 만큼만 확장하도록 MainAxisSize 설정
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                FaIcon(
+                  icon,
+                  color: Colors.white,
+                ),
+                Gaps.v5,
+                Text(
+                  text,
+                  style: const TextStyle(
+                    color: Colors.white,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
