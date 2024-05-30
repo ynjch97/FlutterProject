fab7199bfa54e14b7b48792500733db087a119eb
ynjch97 <ynjch2017@naver.com>
2024-05-30 10:51:44 +0900

    틱톡 클론코딩 > INBOX > 10.4 RotationTransition

--- tiktok_clone/lib/features/inbox/activity_screen.dart
+++ tiktok_clone/lib/features/inbox/activity_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: slash_for_doc_comments
+
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:tiktok_clone/constants/gaps.dart';
@@ -10,9 +12,28 @@ class ActivityScreen extends StatefulWidget {
   State<ActivityScreen> createState() => _ActivityScreenState();
 }
 
-class _ActivityScreenState extends State<ActivityScreen> {
+/**애니메이션 효과
+ * 1. AnimationController 의 value 를 수정 => controller 에 event listener 룰 추가 => setState() 로 rebuild
+ * 2. Animation Builder 사용
+ * 3. 현재 방법 : _animation 과 _animationController 을 연결하기만 함
+ */
+
+// SingleTickerProviderStateMixin : Ticker 를 가져다주고, widget tree 에 없는 widget 때문에 리소스 낭비하는 것을 방지
+class _ActivityScreenState extends State<ActivityScreen>
+    with SingleTickerProviderStateMixin {
   // List.generate : 리스트 생성 -> 20개
   final List<String> _notifications = List.generate(20, (index) => "${index}h");
+
+  // this 를 참조하려면, late 선언 후 initState() 에서 지정하거나, late 선언과 함께 사용해야 함-!
+  late final AnimationController _animationController = AnimationController(
+    vsync: this,
+    duration: const Duration(milliseconds: 200),
+  );
+
+  // 숫자 값이 아닌 다른 애니메이션을 추가 (Color, Position, Turn 등)
+  // Animation<double> 의 자료형과 begin/end 자료형이 동일해야 함
+  late final Animation<double> _animation =
+      Tween(begin: 0.0, end: 0.5).animate(_animationController);
 
   void _onDismissed(String notification) {
     // List 에서 제거
@@ -20,11 +41,35 @@ class _ActivityScreenState extends State<ActivityScreen> {
     setState(() {});
   }
 
+  void _onTitleTap() {
+    if (_animationController.isCompleted) {
+      _animationController.reverse();
+    } else {
+      _animationController.forward();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text("All activity"),
+        title: GestureDetector(
+          onTap: _onTitleTap,
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Text("All activity"),
+              Gaps.h6,
+              RotationTransition(
+                turns: _animation,
+                child: const FaIcon(
+                  FontAwesomeIcons.chevronDown,
+                  size: Sizes.size14,
+                ),
+              )
+            ],
+          ),
+        ),
       ),
       body: ListView(
         children: [
