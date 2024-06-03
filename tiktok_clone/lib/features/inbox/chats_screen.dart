b38b4666dae590ae9b3dfa42936ed90d1dfc9301
ynjch97 <ynjch2017@naver.com>
2024-06-03 09:27:07 +0900

    틱톡 클론코딩 > CHATS > 11.2 AnimatedList

--- tiktok_clone/lib/features/inbox/chats_screen.dart
+++ tiktok_clone/lib/features/inbox/chats_screen.dart
@@ -10,6 +10,22 @@ class ChatsScreen extends StatefulWidget {
 }
 
 class _ChatsScreenState extends State<ChatsScreen> {
+  final GlobalKey<AnimatedListState> _key = GlobalKey<AnimatedListState>();
+
+  final List<int> _items = [];
+
+  void _addItem() {
+    if (_key.currentState != null) {
+      _key.currentState!.insertItem(
+        _items.length,
+        duration: const Duration(
+          milliseconds: 500,
+        ),
+      );
+      _items.add(_items.length);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -18,51 +34,58 @@ class _ChatsScreenState extends State<ChatsScreen> {
         title: const Text("Direct Messages"),
         actions: [
           IconButton(
-            onPressed: () {},
+            onPressed: _addItem,
             icon: const FaIcon(FontAwesomeIcons.plus),
           ),
         ],
       ),
-      /**
-       * - ListView.builder 와 동일한 기능 + 애니메이션을 포함
-       * 
-       */
-      body: ListView(
+      // AnimatedList : ListView.builder 와 동일한 기능 + 애니메이션을 포함
+      body: AnimatedList(
+        key: _key,
         padding: const EdgeInsets.symmetric(vertical: Sizes.size10),
-        children: [
-          ListTile(
-            leading: const CircleAvatar(
-              radius: 30,
-              foregroundImage: NetworkImage(
-                "https://avatars.githubusercontent.com/u/69029517",
-              ),
-              child: Text(
-                "YNJCH",
-                style: TextStyle(fontSize: Sizes.size10),
-              ),
-            ),
-            title: Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              crossAxisAlignment: CrossAxisAlignment.end,
-              children: [
-                const Text(
-                  "YNJCH",
-                  style: TextStyle(
-                    fontWeight: FontWeight.w600,
+        // build 시작할 때 아이템을 몇 개 보여줄 것인지
+        initialItemCount: 0,
+        itemBuilder: (context, index, animation) {
+          return FadeTransition(
+            key: UniqueKey(),
+            opacity: animation,
+            child: SizeTransition(
+              sizeFactor: animation,
+              child: ListTile(
+                leading: const CircleAvatar(
+                  radius: 30,
+                  foregroundImage: NetworkImage(
+                    "https://avatars.githubusercontent.com/u/69029517",
+                  ),
+                  child: Text(
+                    "YNJCH",
+                    style: TextStyle(fontSize: Sizes.size10),
                   ),
                 ),
-                Text(
-                  "2:54 PM",
-                  style: TextStyle(
-                    color: Colors.grey.shade500,
-                    fontSize: Sizes.size12,
-                  ),
+                title: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    Text(
+                      "YNJCH ($index)",
+                      style: const TextStyle(
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
+                    Text(
+                      "2:54 PM",
+                      style: TextStyle(
+                        color: Colors.grey.shade500,
+                        fontSize: Sizes.size12,
+                      ),
+                    ),
+                  ],
                 ),
-              ],
+                subtitle: const Text("Don't forget to make video 🎞️"),
+              ),
             ),
-            subtitle: const Text("Don't forget to make video 🎞️"),
-          ),
-        ],
+          );
+        },
       ),
     );
   }
