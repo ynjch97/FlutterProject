f78fc6efe8c16353bf74d7bb55ff0b260a4cac50
ynjch97 <ynjch2017@naver.com>
2024-06-03 15:52:36 +0900

    틱톡 클론코딩 > USER PROFILE > 12.2 SliverAppBar

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -23,231 +23,41 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: DefaultTabController(
-        length: 2,
-        child: NestedScrollView(
-          headerSliverBuilder: (context, innerBoxIsScrolled) {
-            return [
-              SliverAppBar(
-                title: const Text('YNJCH'),
-                actions: [
-                  IconButton(
-                    onPressed: _onGearPressed,
-                    icon: const FaIcon(
-                      FontAwesomeIcons.gear,
-                      size: Sizes.size20,
-                    ),
-                  )
-                ],
-              ),
-              SliverToBoxAdapter(
-                child: Column(
-                  children: [
-                    const CircleAvatar(
-                      radius: 50,
-                      foregroundImage: NetworkImage(
-                        "https://avatars.githubusercontent.com/u/69029517",
-                      ),
-                      child: Text("YNJCH"),
-                    ),
-                    Gaps.v20,
-                    Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        const Text(
-                          "@ynjch",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                            fontSize: Sizes.size18,
-                          ),
-                        ),
-                        Gaps.h5,
-                        FaIcon(
-                          FontAwesomeIcons.solidCircleCheck,
-                          size: Sizes.size16,
-                          color: Colors.blue.shade500,
-                        ),
-                      ],
-                    ),
-                    Gaps.v24,
-                    SizedBox(
-                      height: Sizes.size50,
-                      child: Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children: [
-                          Column(
-                            children: [
-                              const Text(
-                                "97",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Sizes.size18,
-                                ),
-                              ),
-                              Gaps.v3,
-                              Text(
-                                "Following",
-                                style: TextStyle(
-                                  color: Colors.grey.shade500,
-                                ),
-                              )
-                            ],
-                          ),
-                          // VerticalDivider : SizedBox 로 감싸서 높이 지정을 해줘야 화면에서 볼 수 있음
-                          VerticalDivider(
-                            width: Sizes.size32,
-                            thickness: Sizes.size1,
-                            color: Colors.grey.shade400,
-                            indent: Sizes.size14,
-                            endIndent: Sizes.size14,
-                          ),
-                          Column(
-                            children: [
-                              const Text(
-                                "10M",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Sizes.size18,
-                                ),
-                              ),
-                              Gaps.v3,
-                              Text(
-                                "Followers",
-                                style: TextStyle(
-                                  color: Colors.grey.shade500,
-                                ),
-                              ),
-                            ],
-                          ),
-                          VerticalDivider(
-                            width: Sizes.size32,
-                            thickness: Sizes.size1,
-                            color: Colors.grey.shade400,
-                            indent: Sizes.size14,
-                            endIndent: Sizes.size14,
-                          ),
-                          Column(
-                            children: [
-                              const Text(
-                                "194.3M",
-                                style: TextStyle(
-                                  fontWeight: FontWeight.bold,
-                                  fontSize: Sizes.size18,
-                                ),
-                              ),
-                              Gaps.v3,
-                              Text(
-                                "Likes",
-                                style: TextStyle(
-                                  color: Colors.grey.shade500,
-                                ),
-                              ),
-                            ],
-                          )
-                        ],
-                      ),
-                    ),
-                    Gaps.v14,
-                    FractionallySizedBox(
-                      widthFactor: 0.33,
-                      child: Container(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: Sizes.size12,
-                        ),
-                        decoration: BoxDecoration(
-                          color: Theme.of(context).primaryColor,
-                          borderRadius: const BorderRadius.all(
-                            Radius.circular(Sizes.size4),
-                          ),
-                        ),
-                        child: const Text(
-                          'Follow',
-                          style: TextStyle(
-                            color: Colors.white,
-                            fontWeight: FontWeight.w600,
-                          ),
-                          textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                    Gaps.v14,
-                    const Padding(
-                      padding: EdgeInsets.symmetric(
-                        horizontal: Sizes.size32,
-                      ),
-                      child: Text(
-                        "All highlights and where to watch live matches on FIFA+ I wonder how it would loook",
-                        textAlign: TextAlign.center,
-                      ),
-                    ),
-                    Gaps.v12,
-                    const Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        FaIcon(
-                          FontAwesomeIcons.link,
-                          size: Sizes.size12,
-                        ),
-                        Gaps.h4,
-                        Text(
-                          "https://nomadcoders.co",
-                          style: TextStyle(
-                            fontWeight: FontWeight.w600,
-                          ),
-                        ),
-                      ],
-                    ),
-                    Gaps.v20,
-                  ],
-                ),
-              ),
-              SliverPersistentHeader(
-                // TabBar 사용을 위해 DefaultTabController 로 감싸줘야 함
-                delegate: PersistentTabBar(),
-                pinned: true,
-              ),
-            ];
-          },
-          body: TabBarView(
-            children: [
-              GridView.builder(
-                // GridView.builder 의 스크롤을 막음
-                physics: const NeverScrollableScrollPhysics(),
-                itemCount: 20,
-                padding: EdgeInsets.zero,
-                // gridDelegate : GridView 를 구성하는데 도움을 주는 역할
-                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 3,
-                  crossAxisSpacing: Sizes.size2,
-                  mainAxisSpacing: Sizes.size2,
-                  // 가로/세로 비율
-                  childAspectRatio: 9 / 14,
-                ),
-                itemBuilder: (context, index) => Column(
-                  // Todo : wrap Stack -> 조회수 표시 code challenge
-                  children: [
-                    // 특정한 비율을 따르도록 설정할 수 있음
-                    AspectRatio(
-                      aspectRatio: 9 / 14,
-                      // FadeInImage.assetNetwork : 네트워크에서 이미지를 로딩하는 동안에는 assets 폴더 내의 이미지를 보여줌
-                      child: FadeInImage.assetNetwork(
-                        fit: BoxFit.cover, // 부모 요소에 어떻게 fit 시킬건지
-                        placeholder: "assets/images/kota.jpg",
-                        image:
-                            "https://images.unsplash.com/photo-1673844969019-c99b0c933e90?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1480&q=80",
-                      ),
-                    ),
-                  ],
-                ),
-              ),
-              const Center(
-                child: Text('Page two'),
-              ),
+    return CustomScrollView(
+      // 사용자가 스크롤할 수 있는 특정한 sliver widget 목록을 넣음
+      slivers: [
+        SliverAppBar(
+          // SliverAppBar 가 숨겨지거나 보여지는 방식
+          snap: true, // 살짝만 올라가도 appbar 전체가 바로 보여짐
+          floating: true, // 스크롤을 내렸다가 올라가면 다시 보여짐
+          stretch: true,
+          pinned: true, // FlexibleSpaceBar 를 볼 수 있게 유지
+          backgroundColor: Theme.of(context).primaryColor,
+          // 화면을 쓸어올리면 80px까지 줄어들었다가 사라짐
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: const FlexibleSpaceBar(
+            stretchModes: [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
             ],
           ),
         ),
-      ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            childCount: 50, // 50개 제한
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
+          ),
+          itemExtent: 100, // item 의 height
+        ),
+      ],
     );
   }
 }
