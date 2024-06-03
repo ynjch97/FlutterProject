978d6c1acda0dc00d8bc06139b435658b3448b72
ynjch97 <ynjch2017@naver.com>
2024-06-03 15:52:36 +0900

    틱톡 클론코딩 > USER PROFILE > 12.2 SliverAppBar

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -14,10 +14,12 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       // 사용자가 스크롤할 수 있는 특정한 sliver widget 목록을 넣음
       slivers: [
         SliverAppBar(
-          floating: true,
+          // SliverAppBar 가 숨겨지거나 보여지는 방식
+          snap: true, // 살짝만 올라가도 appbar 전체가 바로 보여짐
+          floating: true, // 스크롤을 내렸다가 올라가면 다시 보여짐
           stretch: true,
-          pinned: true,
-          backgroundColor: Colors.teal,
+          pinned: true, // FlexibleSpaceBar 를 볼 수 있게 유지
+          backgroundColor: Theme.of(context).primaryColor,
           // 화면을 쓸어올리면 80px까지 줄어들었다가 사라짐
           collapsedHeight: 80,
           expandedHeight: 200,
@@ -25,6 +27,7 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             stretchModes: const [
               StretchMode.blurBackground,
               StretchMode.zoomBackground,
+              StretchMode.fadeTitle,
             ],
             background: Image.asset(
               "assets/images/kota.jpg",
@@ -38,9 +41,16 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         ),
         SliverFixedExtentList(
           delegate: SliverChildBuilderDelegate(
-            (context, index) => Container(),
+            childCount: 50, // 50개 제한
+            (context, index) => Container(
+              color: Colors.amber[100 * (index % 9)],
+              child: Align(
+                alignment: Alignment.center,
+                child: Text("Item $index"),
+              ),
+            ),
           ),
-          itemExtent: 100,
+          itemExtent: 100, // item 의 height
         ),
       ],
     );
