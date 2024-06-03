d6384017d5e689d5a3e1757ab119149d4739520d
ynjch97 <ynjch2017@naver.com>
2024-06-03 15:42:41 +0900

    틱톡 클론코딩 > USER PROFILE > 12.1 CustomScrollView

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -10,6 +10,39 @@ class UserProfileScreen extends StatefulWidget {
 class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return const CustomScrollView();
+    return CustomScrollView(
+      // 사용자가 스크롤할 수 있는 특정한 sliver widget 목록을 넣음
+      slivers: [
+        SliverAppBar(
+          floating: true,
+          stretch: true,
+          pinned: true,
+          backgroundColor: Colors.teal,
+          // 화면을 쓸어올리면 80px까지 줄어들었다가 사라짐
+          collapsedHeight: 80,
+          expandedHeight: 200,
+          flexibleSpace: FlexibleSpaceBar(
+            stretchModes: const [
+              StretchMode.blurBackground,
+              StretchMode.zoomBackground,
+            ],
+            background: Image.asset(
+              "assets/images/kota.jpg",
+              fit: BoxFit.cover,
+            ),
+            title: const Text("Hello!"),
+            titlePadding:
+                const EdgeInsets.symmetric(horizontal: 0, vertical: 10),
+            centerTitle: true,
+          ),
+        ),
+        SliverFixedExtentList(
+          delegate: SliverChildBuilderDelegate(
+            (context, index) => Container(),
+          ),
+          itemExtent: 100,
+        ),
+      ],
+    );
   }
 }
