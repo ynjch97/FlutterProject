71db53ce4f5d1091a080008649f74d3a237a144f
ynjch97 <ynjch2017@naver.com>
2024-06-03 09:57:55 +0900

    틱톡 클론코딩 > CHATS > 11.4 Chat Detail

--- tiktok_clone/lib/features/inbox/chat_detail_screen.dart
+++ tiktok_clone/lib/features/inbox/chat_detail_screen.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
+import 'package:tiktok_clone/constants/sizes.dart';
 
 class ChatDetailScreen extends StatefulWidget {
   const ChatDetailScreen({super.key});
@@ -10,6 +13,175 @@ class ChatDetailScreen extends StatefulWidget {
 class _ChatDetailScreenState extends State<ChatDetailScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: ListTile(
+          contentPadding: EdgeInsets.zero,
+          horizontalTitleGap: Sizes.size8,
+          leading: Stack(
+            alignment: AlignmentDirectional.bottomEnd,
+            children: [
+              const CircleAvatar(
+                radius: Sizes.size24,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/69029517",
+                ),
+                child: Text('YJ'),
+              ),
+              SizedBox(
+                height: Sizes.size16,
+                child: Container(
+                  width: Sizes.size16,
+                  decoration: const BoxDecoration(
+                    shape: BoxShape.circle,
+                    color: Colors.white,
+                  ),
+                  child: const Center(
+                    child: FaIcon(
+                      FontAwesomeIcons.solidCircle,
+                      color: Colors.green,
+                      size: Sizes.size10,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          title: const Text(
+            'YNJCH',
+            style: TextStyle(
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          subtitle: const Text('Active now'),
+          trailing: const Row(
+            mainAxisSize: MainAxisSize.min,
+            children: [
+              FaIcon(
+                FontAwesomeIcons.flag,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+              Gaps.h24,
+              FaIcon(
+                FontAwesomeIcons.ellipsis,
+                color: Colors.black,
+                size: Sizes.size20,
+              ),
+            ],
+          ),
+        ),
+      ),
+      body: Stack(
+        children: [
+          ListView.separated(
+            padding: const EdgeInsets.symmetric(
+              vertical: Sizes.size20,
+              horizontal: Sizes.size14,
+            ),
+            itemBuilder: (context, index) {
+              final isMine = index % 2 == 0; // 누구의 메시지인지 구분
+              return Row(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment:
+                    isMine ? MainAxisAlignment.end : MainAxisAlignment.start,
+                children: [
+                  Container(
+                    padding: const EdgeInsets.all(Sizes.size14),
+                    decoration: BoxDecoration(
+                      color:
+                          isMine ? Colors.blue : Theme.of(context).primaryColor,
+                      borderRadius: BorderRadius.only(
+                        topLeft: const Radius.circular(Sizes.size20),
+                        topRight: const Radius.circular(Sizes.size20),
+                        bottomLeft: Radius.circular(
+                          isMine ? Sizes.size20 : Sizes.size5,
+                        ),
+                        bottomRight: Radius.circular(
+                          !isMine ? Sizes.size20 : Sizes.size5,
+                        ),
+                      ),
+                    ),
+                    child: const Text(
+                      "this is a message!",
+                      style: TextStyle(
+                        color: Colors.white,
+                        fontSize: Sizes.size16,
+                      ),
+                    ),
+                  ),
+                ],
+              );
+            },
+            separatorBuilder: (context, index) => Gaps.v10,
+            itemCount: 10,
+          ),
+          Positioned(
+            bottom: 0,
+            width: MediaQuery.of(context).size.width,
+            child: BottomAppBar(
+              elevation: 0,
+              color: Colors.grey.shade100,
+              child: Row(
+                children: [
+                  const Expanded(
+                    child: Padding(
+                      padding: EdgeInsets.all(Sizes.size2),
+                      child: SizedBox(
+                        height: Sizes.size48,
+                        child: TextField(
+                          expands: true,
+                          minLines: null,
+                          maxLines: null,
+                          textInputAction: TextInputAction.newline,
+                          decoration: InputDecoration(
+                            hintText: "Send a message...",
+                            border: OutlineInputBorder(
+                              borderRadius: BorderRadius.all(
+                                  Radius.circular(Sizes.size24)),
+                              borderSide: BorderSide.none,
+                            ),
+                            filled: true,
+                            fillColor: Colors.white,
+                            contentPadding: EdgeInsets.only(left: Sizes.size16),
+                            suffixIcon: Row(
+                              mainAxisSize: MainAxisSize.min,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                FaIcon(
+                                  FontAwesomeIcons.faceSmile,
+                                  color: Colors.black,
+                                  size: Sizes.size24,
+                                ),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Gaps.h20,
+                  Container(
+                    decoration: BoxDecoration(
+                      border: Border.all(
+                        width: Sizes.size10,
+                        color: Colors.grey.shade300,
+                      ),
+                      shape: BoxShape.circle,
+                      color: Colors.grey.shade300,
+                    ),
+                    child: const FaIcon(
+                      FontAwesomeIcons.paperPlane,
+                      color: Colors.black,
+                      size: Sizes.size20,
+                    ),
+                  )
+                ],
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
