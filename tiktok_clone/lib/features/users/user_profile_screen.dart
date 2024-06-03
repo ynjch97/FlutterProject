8d9c23f226abc8a6299cdfc31340abab44f4d367
ynjch97 <ynjch2017@naver.com>
2024-06-03 16:25:33 +0900

    틱톡 클론코딩 > USER PROFILE > 12.4 SliverPersistentHeader

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -12,14 +12,14 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
   @override
   Widget build(BuildContext context) {
     return CustomScrollView(
-      // 사용자가 스크롤할 수 있는 특정한 sliver widget 목록을 넣음
+      // 사용자가 스크롤할 수 있는 구역 => 특정한 sliver widget 목록을 넣음
       slivers: [
         SliverAppBar(
           // SliverAppBar 가 숨겨지거나 보여지는 방식
           // snap: true, // 살짝만 올라가도 appbar 전체가 바로 보여짐
           // floating: true, // 스크롤을 내렸다가 올라가면 다시 보여짐
           stretch: true,
-          pinned: true, // FlexibleSpaceBar 를 볼 수 있게 유지
+          // pinned: true, // FlexibleSpaceBar 를 볼 수 있게 유지
           backgroundColor: Theme.of(context).primaryColor,
           // 화면을 쓸어올리면 80px까지 줄어들었다가 사라짐
           collapsedHeight: 80,
@@ -29,6 +29,17 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
               StretchMode.blurBackground,
               StretchMode.zoomBackground,
               StretchMode.fadeTitle,
+            ],
+          ),
+        ),
+        // SliverToBoxAdapter : 일반적인 Flutter Widget 을 render 할 때 사용
+        const SliverToBoxAdapter(
+          child: Column(
+            children: [
+              CircleAvatar(
+                backgroundColor: Colors.red,
+                radius: 20,
+              )
             ],
           ),
         ),
@@ -44,6 +55,10 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
             ),
           ),
           itemExtent: 100, // item 의 height
+        ),
+        SliverPersistentHeader(
+          delegate: CustomDelegate(),
+          pinned: true,
         ),
         SliverGrid(
           delegate: SliverChildBuilderDelegate(
@@ -66,5 +81,42 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
         ),
       ],
     );
+  }
+}
+
+// SliverPersistentHeaderDelegate 를 extends 해서 사용해야 함
+class CustomDelegate extends SliverPersistentHeaderDelegate {
+  @override
+  Widget build(
+      BuildContext context, double shrinkOffset, bool overlapsContent) {
+    return Container(
+      color: Colors.lightGreen,
+      // FractionallySizedBox : 부모로부터 최대한 많은 공간을 차지
+      child: const FractionallySizedBox(
+        heightFactor: 1, // 100% (maxExtent)
+        child: Center(
+          child: Text(
+            'SliverPersistentHeader!!!!!',
+            style: TextStyle(
+              color: Colors.white,
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  double get maxExtent => 150; // 최고 높이
+
+  @override
+  double get minExtent => 80; // 최저 높이
+
+  // SliverPersistentHeader 가 보여져야 되는지 알려주는 method
+  @override
+  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
+    // maxExtent, minExtent 변경 시에는 true 반환
+    // build 에서 완전히 다른 widget tree 리턴 시 false 반환
+    return false;
   }
 }
