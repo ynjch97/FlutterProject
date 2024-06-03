ba80b6b051f531d2a6779695743bd7c8d038f7ea
ynjch97 <ynjch2017@naver.com>
2024-06-03 19:23:59 +0900

    틱톡 클론코딩 > SETTINGS > 13.2 showDateRangePicker

--- tiktok_clone/lib/features/settings/settings_screen.dart
+++ tiktok_clone/lib/features/settings/settings_screen.dart
@@ -3,9 +3,14 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-class SettingsScreen extends StatelessWidget {
+class SettingsScreen extends StatefulWidget {
   const SettingsScreen({super.key});
 
+  @override
+  State<SettingsScreen> createState() => _SettingsScreenState();
+}
+
+class _SettingsScreenState extends State<SettingsScreen> {
   /**CloseButton() : 이 코드만으로도 닫기 버튼을 만들 수 있음 
    * CupertinoActivityIndicator(), CircularProgressIndicator() : 로딩 중임을 표시
    * CircularProgressIndicator.adaptive() : 안드로이드, iOS 환경에 맞는 아이콘으로 표시함
@@ -37,6 +42,42 @@ class SettingsScreen extends StatelessWidget {
             subtitle: const Text("About this app....."),
           ),
           const AboutListTile(), // showAboutDialog 를 자동으로 생성
+          // 날짜 선택 기능 추가
+          ListTile(
+            onTap: () async {
+              // showDatePicker, showTimePicker, showDateRangePicker : 아무것도 고르지 않으면 null 반환
+              final date = await showDatePicker(
+                context: context,
+                initialDate: DateTime.now(),
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+              );
+              print(date);
+              if (!context.mounted) return;
+              final time = await showTimePicker(
+                context: context,
+                initialTime: TimeOfDay.now(),
+              );
+              print(time);
+              if (!context.mounted) return;
+              final booking = await showDateRangePicker(
+                context: context,
+                firstDate: DateTime(1980),
+                lastDate: DateTime(2030),
+                builder: (context, child) {
+                  return Theme(
+                    data: ThemeData(
+                        appBarTheme: const AppBarTheme(
+                            foregroundColor: Colors.white,
+                            backgroundColor: Colors.black)),
+                    child: child!,
+                  );
+                },
+              );
+              print(booking);
+            },
+            title: const Text("What is your birthday?"),
+          )
         ],
       ),
     );
