ae466cd0743bb94f4d1a1560786d22e7c641c5bd
ynjch97 <ynjch2017@naver.com>
2024-06-03 17:04:10 +0900

    틱톡 클론코딩 > USER PROFILE > 12.5 VerticalDivider

--- tiktok_clone/lib/features/users/user_profile_screen.dart
+++ tiktok_clone/lib/features/users/user_profile_screen.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:tiktok_clone/constants/gaps.dart';
 import 'package:tiktok_clone/constants/sizes.dart';
 
 class UserProfileScreen extends StatefulWidget {
@@ -15,108 +17,131 @@ class _UserProfileScreenState extends State<UserProfileScreen> {
       // 사용자가 스크롤할 수 있는 구역 => 특정한 sliver widget 목록을 넣음
       slivers: [
         SliverAppBar(
-          // SliverAppBar 가 숨겨지거나 보여지는 방식
-          // snap: true, // 살짝만 올라가도 appbar 전체가 바로 보여짐
-          // floating: true, // 스크롤을 내렸다가 올라가면 다시 보여짐
-          stretch: true,
-          // pinned: true, // FlexibleSpaceBar 를 볼 수 있게 유지
-          backgroundColor: Theme.of(context).primaryColor,
-          // 화면을 쓸어올리면 80px까지 줄어들었다가 사라짐
-          collapsedHeight: 80,
-          expandedHeight: 200,
-          flexibleSpace: const FlexibleSpaceBar(
-            stretchModes: [
-              StretchMode.blurBackground,
-              StretchMode.zoomBackground,
-              StretchMode.fadeTitle,
-            ],
-          ),
+          title: const Text('YNJCH'),
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const FaIcon(
+                FontAwesomeIcons.gear,
+                size: Sizes.size20,
+              ),
+            )
+          ],
         ),
-        // SliverToBoxAdapter : 일반적인 Flutter Widget 을 render 할 때 사용
-        const SliverToBoxAdapter(
+        SliverToBoxAdapter(
           child: Column(
             children: [
-              CircleAvatar(
-                backgroundColor: Colors.red,
-                radius: 20,
-              )
+              const CircleAvatar(
+                radius: 50,
+                foregroundImage: NetworkImage(
+                  "https://avatars.githubusercontent.com/u/69029517",
+                ),
+                child: Text("YNJCH"),
+              ),
+              Gaps.v20,
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  const Text(
+                    "@ynjch",
+                    style: TextStyle(
+                      fontWeight: FontWeight.w600,
+                      fontSize: Sizes.size18,
+                    ),
+                  ),
+                  Gaps.h5,
+                  FaIcon(
+                    FontAwesomeIcons.solidCircleCheck,
+                    size: Sizes.size16,
+                    color: Colors.blue.shade500,
+                  ),
+                ],
+              ),
+              Gaps.v24,
+              SizedBox(
+                height: Sizes.size48,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Column(
+                      children: [
+                        const Text(
+                          "97",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size16,
+                          ),
+                        ),
+                        Gaps.v3,
+                        Text(
+                          "Following",
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontSize: Sizes.size12,
+                          ),
+                        )
+                      ],
+                    ),
+                    // VerticalDivider : SizedBox 로 감싸서 높이 지정을 해줘야 화면에서 볼 수 있음
+                    VerticalDivider(
+                      width: Sizes.size32,
+                      thickness: Sizes.size1,
+                      color: Colors.grey.shade400,
+                      indent: Sizes.size14,
+                      endIndent: Sizes.size14,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          "10M",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size16,
+                          ),
+                        ),
+                        Gaps.v3,
+                        Text(
+                          "Followers",
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontSize: Sizes.size12,
+                          ),
+                        ),
+                      ],
+                    ),
+                    VerticalDivider(
+                      width: Sizes.size32,
+                      thickness: Sizes.size1,
+                      color: Colors.grey.shade400,
+                      indent: Sizes.size14,
+                      endIndent: Sizes.size14,
+                    ),
+                    Column(
+                      children: [
+                        const Text(
+                          "194.3M",
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: Sizes.size16,
+                          ),
+                        ),
+                        Gaps.v3,
+                        Text(
+                          "Likes",
+                          style: TextStyle(
+                            color: Colors.grey.shade500,
+                            fontSize: Sizes.size12,
+                          ),
+                        ),
+                      ],
+                    )
+                  ],
+                ),
+              ),
             ],
           ),
-        ),
-        SliverFixedExtentList(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 20, // 20개 제한
-            (context, index) => Container(
-              color: Colors.amber[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("Item $index"),
-              ),
-            ),
-          ),
-          itemExtent: 100, // item 의 height
-        ),
-        SliverPersistentHeader(
-          delegate: CustomDelegate(),
-          pinned: true,
-        ),
-        SliverGrid(
-          delegate: SliverChildBuilderDelegate(
-            childCount: 50, // 50개 제한
-            (context, index) => Container(
-              color: Colors.blue[100 * (index % 9)],
-              child: Align(
-                alignment: Alignment.center,
-                child: Text("Item $index"),
-              ),
-            ),
-          ),
-          // 허용된 만큼의 무한한 grid 를 생성
-          gridDelegate: const SliverGridDelegateWithMaxCrossAxisExtent(
-            maxCrossAxisExtent: 100,
-            mainAxisSpacing: Sizes.size20,
-            crossAxisSpacing: Sizes.size20,
-            childAspectRatio: 1,
-          ),
-        ),
+        )
       ],
     );
-  }
-}
-
-// SliverPersistentHeaderDelegate 를 extends 해서 사용해야 함
-class CustomDelegate extends SliverPersistentHeaderDelegate {
-  @override
-  Widget build(
-      BuildContext context, double shrinkOffset, bool overlapsContent) {
-    return Container(
-      color: Colors.lightGreen,
-      // FractionallySizedBox : 부모로부터 최대한 많은 공간을 차지
-      child: const FractionallySizedBox(
-        heightFactor: 1, // 100% (maxExtent)
-        child: Center(
-          child: Text(
-            'SliverPersistentHeader!!!!!',
-            style: TextStyle(
-              color: Colors.white,
-            ),
-          ),
-        ),
-      ),
-    );
-  }
-
-  @override
-  double get maxExtent => 150; // 최고 높이
-
-  @override
-  double get minExtent => 80; // 최저 높이
-
-  // SliverPersistentHeader 가 보여져야 되는지 알려주는 method
-  @override
-  bool shouldRebuild(covariant SliverPersistentHeaderDelegate oldDelegate) {
-    // maxExtent, minExtent 변경 시에는 true 반환
-    // build 에서 완전히 다른 widget tree 리턴 시 false 반환
-    return false;
   }
 }
