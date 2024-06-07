214f81865cacd51d1dfc2af1b2d6b51b7f28f0ae
ynjch97 <ynjch2017@naver.com>
2024-06-07 17:47:42 +0900

    틱톡 클론코딩 > VIDEO RECORDING > 19.7 GallerySaver

--- tiktok_clone/lib/features/videos/video_preview_screen.dart
+++ tiktok_clone/lib/features/videos/video_preview_screen.dart
@@ -2,6 +2,8 @@ import 'dart:io';
 
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+import 'package:gallery_saver/gallery_saver.dart';
 import 'package:video_player/video_player.dart';
 
 class VideoPreviewScreen extends StatefulWidget {
@@ -17,6 +19,8 @@ class VideoPreviewScreen extends StatefulWidget {
 }
 
 class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
+  bool _isSavedVideo = false;
+
   late final VideoPlayerController _videoPlayerController;
 
   @override
@@ -38,12 +42,34 @@ class _VideoPreviewScreenState extends State<VideoPreviewScreen> {
     setState(() {});
   }
 
+  Future<void> _saveToGallery() async {
+    if (_isSavedVideo) return;
+
+    // 파일 저장 전, 권한 요청이 수락되어야 함
+    await GallerySaver.saveVideo(
+      widget.video.path,
+      albumName: "TikTok Clone", // 저장할 앨범 위치
+    );
+
+    _isSavedVideo = true;
+
+    setState(() {});
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.black,
       appBar: AppBar(
         title: const Text("Preview Video"),
+        actions: [
+          IconButton(
+            onPressed: _saveToGallery,
+            icon: FaIcon(_isSavedVideo
+                ? FontAwesomeIcons.check
+                : FontAwesomeIcons.download),
+          )
+        ],
       ),
       body: _videoPlayerController.value.isInitialized
           ? VideoPlayer(_videoPlayerController)
